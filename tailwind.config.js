995bc63f6e1a258f5570cf26c25dcb418d60ff9d
Varun-Kolanu <112728411+Varun-Kolanu@users.noreply.github.com>
2023-06-01 11:40:15 +0530

    Home Page made

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export const content = ["./dist/**/*.html"];
 export const theme = {
-  extend: {},
+  extend: {
+    backgroundImage: {
+      'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+    }
+  },
 };
 export const plugins = [];
